ce9b8e6b68d646b85bf29aa8074ac4422d76af74
sala03 <sala03@targettrust.local>
2016-04-22 19:15:28 -0300

    JPA Aula 2

---
+++ aula-02-01/src/main/java/br/com/tt/model/Aluno.java
@@ -0,0 +1,78 @@
+package br.com.tt.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name ="Alunos")
+@SequenceGenerator(name="aluno_seq", sequenceName="aluno_seq",allocationSize =1)
+public class Aluno implements Serializable{
+	@Id
+	@GeneratedValue(generator="aluno_seq", strategy= GenerationType.SEQUENCE)
+	private Long id;
+	private String Nome;
+	
+	
+	@Column(name="doc_cpf",length = 11)
+	private String cpf;
+	
+	@Column(name="dt_nascimento")
+	@Temporal(TemporalType.DATE)
+	private Date dataNasc;
+	
+	@Transient  
+	private Integer idade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+
+}
