3605aa8d2877ed2ace58dd84261ed075afa318f3
sala03 <sala03@targettrust.local>
2016-04-27 19:16:02 -0300

    JPA Aula 4

--- aula-04-03-joined/src/main/java/br/com/tt/model/Pessoa.java
+++ aula-04-03-joined/src/main/java/br/com/tt/model/Pessoa.java
@@ -11,6 +11,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.SequenceGenerator;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
