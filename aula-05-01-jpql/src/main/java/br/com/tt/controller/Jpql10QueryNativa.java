f3b20070a873d2d9e08f9a9a79c8d94ff828e1df
sala03 <sala03@targettrust.local>
2016-04-27 22:15:01 -0300

    JPA Aula 5

---
+++ aula-05-01-jpql/src/main/java/br/com/tt/controller/Jpql10QueryNativa.java
@@ -0,0 +1,38 @@
+package br.com.tt.controller;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import br.com.tt.model.Cliente;
+import br.com.tt.vo.ClienteVO;
+
+public class Jpql10QueryNativa {
+
+	public static void main(String[] args) {
+		
+		EntityManager em = getEM();
+		Class<ClienteVO> clazz = ClienteVO.class;
+		String sql =  "SELECT * FROM CLIENTE ";
+
+		Query query = em.createNativeQuery(sql, clazz);
+		List<Cliente> clientes = query.getResultList();
+		clientes.forEach(c-> out.println(c));
+		
+	}
+	
+	private static EntityManager getEM(){
+		return Persistence
+				.createEntityManagerFactory("pu_jpa")
+				.createEntityManager();
+	}
+
+	
+
+}
